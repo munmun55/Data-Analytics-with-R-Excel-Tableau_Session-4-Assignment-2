f1d0f4ad9c89bab6cc4f0afd6c93ce909b5e005a
munmun55 <44106801+munmun55@users.noreply.github.com>
2019-01-08 23:08:58 +0530

    Add files via upload

---
+++ Assignment 4_2.r
@@ -0,0 +1,20 @@
+# Assignment 4.2
+#Solution 1
+
+x <- c('data.science.in.R', 'machine.learning.in.R')
+x 
+#Perform the below string Operation:
+#Replace the period character "." within each string with another character i.e. "-" minus sign.
+
+gsub("\\.",'-', x)
+
+#Solution 2
+
+x <- c('data.science.in.R','machine.learning.in.R')
+x
+#Perform the below String Operation:
+#Append again with "-" minus sign character at the start of the each string and finally concatenate all the
+#string within the vector to form a final single string and assigning it the object.
+
+y <- paste("-", x, collapse = "")
+y
